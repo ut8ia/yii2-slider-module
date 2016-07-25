4f121663dd616f374bec4d92566dae68b64c93e3
root-- <eugene.anufriev@gmail.com>
2016-07-25 19:07:29 +0300

    fix styleles

---
+++ migrations/m160725_170507_create_tables_for_slidermanager.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+
+class m160725_170507_create_tables_for_slidermanager extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160725_170507_create_tables_for_slidermanager cannot be reverted.\n";
+
+        return false;
+    }
+
+}
